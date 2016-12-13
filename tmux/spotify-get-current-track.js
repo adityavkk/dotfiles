39acca58f0f68583e8105ca5545cb4905a8d998a
Aditya K <adityavkk@gmail.com>
2016-12-13 13:14:32 -0500

    add node spotify song finder

---
+++ tmux/spotify-get-current-track.js
@@ -0,0 +1,10 @@
+const nodeSpotifyWebHelper = require('node-spotify-webhelper');
+const spotify = new nodeSpotifyWebHelper.SpotifyWebHelper({port: '4371'});
+// get the name of the song which is currently playing
+spotify.getStatus(function (err, res) {
+  if (err) {
+    return console.error(err);
+  }
+  const song = res.track.artist_resource.name + ' - ' + res.track.track_resource.name;
+  console.log(song.substring(0, 32));
+});
